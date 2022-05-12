db9d2cc5da905257530c533cb2e20082a65bfb62
Cami0807 <camilacaracciolo@hotmail.com>
2022-05-12 18:54:03 -0300

    carpeta proyect nuevo

--- PROYECTO2/app.js
+++ PROYECTO2/app.js
@@ -4,3 +4,12 @@ const app = express ();
 
 
 app.listen(3000, ()=> console.log ("fue Exitoso"));
+
+const path = require ("path");
+
+app.get ("/404", (req,res) => {
+    res.send ("Error página no encontrada");
+    });
+    app.get ("/", (req,res) => {
+    res.sendFiles(path.join(__dirname, "/views/home.html"));
+    });
