7ecbbc52b16ec7e9739dd03099e6bef4f52358b5
Cami0807 <camilacaracciolo@hotmail.com>
2022-05-15 00:10:39 -0300

    con css

--- proyecto1/app.js
+++ proyecto1/app.js
@@ -10,7 +10,6 @@ app.use(express.static(publicPath));
 
 app.listen(3000, () => console.log("esto fue exitoso"));
 
-    
 
 app.get('/home', function(req, res){
     let htmlPath = path.resolve(__dirname, './views/home.html');
